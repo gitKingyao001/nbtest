4b536be35ebc5c85fe344c5d5c42c220f40a9d2b
jayvee.yjw <jayvee.yjw@alibaba-inc.com>
2019-02-25 16:17:34 +0800

    mv jpFinds() to jsonpath.py

--- nbtest/Utils.py
+++ nbtest/Utils.py
@@ -4,7 +4,6 @@ import types, numbers, inspect, traceback, time, chardet, re, os, json
 import subprocess
 from datetime import datetime, timedelta
 from DictObject import DictObject
-import jsonpath_rw_ext as jpath  # pip install jsonpath_rw_ext
 
 from .future2to3 import *
 from .assertpyx import AX, _Utils
@@ -218,29 +217,7 @@ def json_o2t(o, ensure_ascii=False, indent=None, **kw):
     return json.dumps(o, ensure_ascii=ensure_ascii, indent=indent, **kw)
 
 
-def jpFinds(jsDc, jsPath, onlyone=None, notNull=False, onlyoneAuto=True, dftRaise=True, dft=Undef):
-    """
-        print jsFinds({'foo': [{'baz': 1}, {'baz': 2}]}, 'foo[*].baz') #=> [1, 2]
-        print jsFinds({'foo': [{'baz': 1}, {'baz': 2}]}, 'foo[0].baz') #=> 1
-    """
-    #
-    if (not jsDc) or (not jsPath):
-        raise Exception('#(not jsDc) or (not jsPath), jsDc={!r}, jsPath={!r}'.format(jsDc, jsPath))
-    try:
-        jsonpath_expr = jpath.parse(jsPath)
-        res = [match.value for match in jsonpath_expr.find(jsDc)]
-        if len(res) == 0 and notNull:
-            raise Exception('#len(res)==0')
-        if onlyone or (onlyone == None and onlyoneAuto and (('*' not in jsPath) and ('?' not in jsPath))):
-            if len(res) == 1:
-                return res[0]
-            if dftRaise:
-                assert False, 'assert len(res)==1, but res={!r}'.format(res)
-            else:
-                return dft
-        return res
-    except Exception as errObj:
-        raise Exception('jsPath=%r err=%s' % (jsPath, errObj))
+
 
 
 def stackUpFind__Symbol__():
